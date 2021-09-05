7cceb6061a702e3af02c889575ce80b303b023aa
Amal <amalmsp64@gmail.com>
2021-09-05 18:01:41 +0530

    Fixed-routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Content from "./Home/Content"
-import Footer from "./Home/Footer"
-import { Header } from "./Home/Header"
-import Hero from "./Home/Hero"
+import Content from "./Home/Content";
+import Footer from "./Home/Footer";
+import { Header } from "./Home/Header";
+import Hero from "./Home/Hero";
+import NriRegister from "./components/NriRegister";
+import {Switch, Route} from "react-router-dom";
+
+
 function App() {
   return (
     <>
-    <Header />
-    <Content />
-    <Hero />
-    <Footer /> 
+      <Switch>
+        <Route path="/" exact>
+          <Header />
+          <Content />
+          <Hero />
+          <Footer />
+        </Route>
+        <Route path="/nri" component={NriRegister} />
+      </Switch>
     </>
   );
 }
--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 function Footer() {
   return (
     <div className="w-screen h-58 flex flex-col items-center  justify-center text-gray-100 bg-gray-800">
       <p className="text-2xl text-cene font-light">Copyright © 2021 MITS-AMS</p>
       <p className="text-2xl text-cene font-light">Support | 000-000-000</p>
-      <a href="www.mgmits.ac.in" className="text-2xl font-light">www.mgmits.ac.in</a>
+      <a href="www.mgits.ac.in" className="text-2xl font-light">www.mgits.ac.in</a>
     </div>
   );
 }
