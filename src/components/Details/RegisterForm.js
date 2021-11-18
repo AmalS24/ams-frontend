bbc6eb483eba1dc9acdbfb5a9446c30de389dea3
NANDAKRISHNAN P N <nandakrishnanunni01@gmail.com>
2021-11-18 20:10:42 +0530

    Updated details sample page blank

--- src/components/Details/RegisterForm.js
+++ src/components/Details/RegisterForm.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function RegisterForm() {
-
-  // function addHyphen (element) {
-  //   let ele = document.getElementById(element.id);
-  //         ele = ele.value.split('-').join('');    // Remove dash (-) if mistakenly entered.
-  
-  //         let finalVal = ele.match(/.{1,3}/g).join('-');
-  //         document.getElementById(element.id).value = finalVal;
-  // }
-
-  
+function RegisterForm() {  
   return (
-    <div className="flex justify-center items-center xl:w-3/5 w-full">
+    <div className="flex justify-center overflow-scroll-y h-screen items-center  w-full">
       <form
         action=""
-        className="flex flex-col  xs:w-74 sm:w-97.85 sm:h-98 h-full rounded-sm bg-white"
+        className="flex flex-col  sm:w-3/5  h-full  rounded-sm bg-white"
       >
         {/*  */}
       </form>
