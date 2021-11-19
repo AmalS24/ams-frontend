f36d5e0fb10cb91812b4b7e15e0bb76d8175292c
Amal <amalmsp64@gmail.com>
2021-11-19 22:53:02 +0530

    parent-info-page-done

--- src/components/Details/ParentInfoPage.js
+++ src/components/Details/ParentInfoPage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ParentInfoPage() {
   return (
     <div className="w-screen h-full bg-transparent ">
       <form
         action=""
-        className="w-200 flex flex-col items-center py-3 space-y-2 justify-start mx-auto mt-4 left-10 h-99 rounded-sm bg-white"
+        className="w-200 flex flex-col items-center py-3 space-y-2 justify-start mx-auto  left-10 h-99 rounded-sm bg-white"
       >
         <h1 className="text-2xl underline uppercase mb-3 font-semibold">
           Parental Details
@@ -64,6 +65,7 @@ function ParentInfoPage() {
             <input
               type="text"
               className="w-72 h-10 ml-4 pl-4 text-lg italic rounded-full border-2 border-black "
+              placeholder='Not-Mandatory'
             />
             <label htmlFor="" className="text-xl ml-8">
               Relation with Sponser
@@ -71,6 +73,7 @@ function ParentInfoPage() {
             <input
               type="text"
               className="w-72 h-10 ml-4 pl-4 text-lg italic required rounded-full border-2 border-black "
+              placeholder="Not-Mandatory"
             />
             <label htmlFor="" className="text-xl ml-8">
               Annual Income*
@@ -78,6 +81,7 @@ function ParentInfoPage() {
             <input
               type="text"
               className="w-72 h-10 ml-4 pl-4 text-lg required italic rounded-full border-2 border-black "
+              placeholder='------------In-Rupees-----------'
             />
           </div>
         </div>
@@ -107,7 +111,7 @@ function ParentInfoPage() {
                 </div>
                 <div className="flex space-x-2">
                   <label htmlFor="" className="text-xl ">
-                    2.
+                    3.
                   </label>
                   <select
                     name="branch"
@@ -115,16 +119,16 @@ function ParentInfoPage() {
                     id="branch"
                     required
                   >
-                     <option value="CSE">Computer Science</option>
+                    <option value="CSE">Computer Science</option>
                     <option value="EEE">Electrical And Electronics</option>
                     <option value="ECE">Electronics And Communications</option>
                     <option value="MECH">Mechanical</option>
                     <option value="CIVIL">Civil</option>
                   </select>
                 </div>
-                <div className="flex space-x-2">
+                <div className="flex ml-44 space-x-2">
                   <label htmlFor="" className="text-xl ">
-                    3.
+                    5.
                   </label>
                   <select
                     name="branch"
@@ -141,9 +145,9 @@ function ParentInfoPage() {
                 </div>
               </div>
               <div className="flex space-y-8 flex-col h-full">
-              <div className="flex pt-14 space-x-2">
+                <div className="flex pt-14 space-x-2">
                   <label htmlFor="" className="text-xl ">
-                    4.
+                    2.
                   </label>
                   <select
                     name="branch"
@@ -160,7 +164,7 @@ function ParentInfoPage() {
                 </div>
                 <div className="flex space-x-2">
                   <label htmlFor="" className="text-xl ">
-                    5.
+                    4.
                   </label>
                   <select
                     name="branch"
@@ -168,7 +172,7 @@ function ParentInfoPage() {
                     id="branch"
                     required
                   >
-                     <option value="CSE">Computer Science</option>
+                    <option value="CSE">Computer Science</option>
                     <option value="EEE">Electrical And Electronics</option>
                     <option value="ECE">Electronics And Communications</option>
                     <option value="MECH">Mechanical</option>
@@ -178,10 +182,27 @@ function ParentInfoPage() {
               </div>
             </div>
             <div className="h-full flex  space-y-8  flex-col">
-              <h1 className="text-xl font-bold ml-6 underline  animate-pulse">Current Status</h1>
-              <div className="w-80 mx-auto h-40 flex items-center justify-center  rounded-xl border-2 border-black">
-              <p className="text-xl text-center text-torch-red-500 italic">We've received your request await for confirmation</p>
+              <h1 className="text-xl font-bold ml-6 underline ">
+                Current Status
+              </h1>
+              <div className="w-80 mx-auto bg-red-50 h-40 flex items-center justify-center  rounded-xl border-2 border-black">
+                <p className="text-xl text-center animate-pulse text-torch-red-500 italic">
+                  We've received your request await for confirmation
+                </p>
               </div>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-between h-1/5">
+            <Link className="h-11 w-30 flex items-center justify-center rounded-full border-2 text-torch-red-500 text-xl border-torch-red-500 hover:text-white hover:bg-torch-red-500">
+              Back
+            </Link>
+            <div className="w-94 flex px-5 items-center justify-between h-full">
+              <Link className="h-11 w-30 ml-14 flex items-center justify-center rounded-full border-2 text-torch-red-500 text-xl border-torch-red-500 hover:text-white hover:bg-torch-red-500">
+                Save
+              </Link>
+              <Link className="h-11 w-30 flex items-center justify-center rounded-full border-2 text-torch-red-500 text-xl border-torch-red-500 hover:text-white hover:bg-torch-red-500">
+                Next
+              </Link>
             </div>
           </div>
         </div>
